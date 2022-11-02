a5248361d229f82458a093d33b17a5abf46c0904
Maninder Singh <smaninder403@gmail.com>
2022-11-01 23:30:57 -0400

    removed some code from code php

--- php/code.php
+++ php/code.php
@@ -99,35 +99,3 @@ if(isset($_POST['delete_btn'])) {
         exit(0);
     }
 }
-
-//CREATE DEPTARMENT 
-
-if(isset($_POST['create_btn'])){
-    $deptid = $_POST['deptid'];
-    $deptname = $_POST['deptname'];
-    $deptemail = $_POST['deptemail'];
-    $buildingid = $_POST['buildingid'];
-    $roomid = $_POST['roomid'];
-    $deptphone = $_POST['deptphone'];
-    $deptchair = $_POST['deptchair'];
-    $deptmg = $_POST['deptmg'];
-
-    $query= "INSERT INTO department 
-    (deptid , deptname, deptemail, buildingif, roomid, deptphone, deptchair, deptmg )
-    VALUES ('$deptid, $deptname, $deptemail, $buildingid, $roomid, $deptphone, $deptchair, $deptmg')";
-    $query_run = mysqli_query($connection, $query);
-
-    if(mysqli_num_rows($deptid_query_run) > 0){
-        $_SESSION['status'] = "Deptartment ID Already Taken. Please Try Another one.";
-        $_SESSION['status_code'] = "error";
-        header('Location: ..//navigations/admin/index.php');
-    }
-    else if($query_run) {
-        $_SESSION['success'] = "Department has been Created";
-        header('Location: ../navigations/admin/adminviewUser.php');
-    }
-    else {
-        $_SESSION['status'] = "Department could not be created";
-        header('Location: ../index.php');
-    }
-}
