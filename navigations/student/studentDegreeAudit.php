876091fdaf5c5eb5ca3e2196d94015144238a98e
mwill100 <115943922+mwill100@users.noreply.github.com>
2022-11-02 23:51:27 -0400

    started body of degree aduit, more tmm

--- navigations/student/studentDegreeAudit.php
+++ navigations/student/studentDegreeAudit.php
@@ -10,3 +10,27 @@ $studentId = $_SESSION['userid'];
 
     echo $studentId;
 ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Academic Calendar</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+    <div class="container">
+      <a href="../index.php" class="navbar-brand">Money Making University</a>
+    </div>
+  </nav>
+</head>
+<body>
+<br /><br />
+  <div class="container">
+    <h3 align="center">Student Degree Aduit</h3>
+    <br />
+</body> 
