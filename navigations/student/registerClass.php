d8555b9988af206740cc61072f9f3eb03ace7fee
Maninder Singh <smaninder403@gmail.com>
2022-12-02 23:04:03 -0500

    tests

--- navigations/student/registerClass.php
+++ navigations/student/registerClass.php
@@ -10,26 +10,30 @@ if (isset($_POST['register_btn'])) {
     $studentid = $_SESSION['id'];
     $crn = $_POST['crn'];
     $courseid = $_POST['courseid'];
-    $dateenrolled = date('y-m-d');
+    $dateenrolled = '2022-12-20';
     $semyear = $_POST['semyear'];
     $grade = 'IP';
 
-    $query = "INSERT INTO enrollment (studentid, crn, courseid, dateenrolled, semyear, grade)
-    VALUES ('$studentid', '$crn', '$courseid', '$dateenrolled', '$semyear', '$grade')";
+    $query = "SELECT * FROM enrollment WHERE enrollment.studentid = $studentid";
     $query_run = mysqli_query($connection, $query);
-
-    if (mysqli_num_rows($semyear_query_run) != 'S2023') {
-        $_SESSION['status'] = "Cannot Register for this Semester Year";
-        $_SESSION['status_code'] = "error";
-        header('Location: ./registration.php');
-        exit(0);
-    } else if ($query_run) {
-        $_SESSION['success'] = "Sucessfully Registered for Class";
+    $row = mysqli_fetch_array($query_run);
+    if ($row['studentid'] == $studentid && $row['courseid'] == $courseid) {
+        $_SESSION['status'] = "Cannot Register for a Course that is already registered";
         header('Location: ./registration.php');
         exit(0);
     } else {
-        $_SESSION['status'] = "Couldn't Register for Class";
-        header('Location: ./registration.php');
-        exit(0);
+        $query = "INSERT INTO enrollment (studentid, crn, courseid, dateenrolled, semyear, grade)
+    VALUES ('$studentid', '$crn', '$courseid', '$dateenrolled', '$semyear', '$grade')";
+        $query_run = mysqli_query($connection, $query);
+
+        if ($query_run) {
+            $_SESSION['success'] = "Sucessfully Registered for Class";
+            header('Location: ./registration.php');
+            exit(0);
+        } else {
+            $_SESSION['status'] = "Couldn't Register for Class";
+            header('Location: ./registration.php');
+            exit(0);
+        }
     }
 }
