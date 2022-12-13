a57b354fb112d0b7c9ed7038ef0a8e955664e10f
Maninder Singh <smaninder403@gmail.com>
2022-12-12 23:23:14 -0500

    added fac

---
+++ navigations/admin/adminviewFaculty.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+include "../config.php";
+if (!isset($_SESSION['id'])) {
+    header("Location: ../login.php");
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Faculty Information</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Admin Homepage</a>
+        </div>
+        <div class="container-fluid">
+            <a class="btn btn-lg btn-danger" href="../logout.php" role="button">Logout</a>
+        </div>
+    </nav>
+</head>
+
+<body>
+    <br /><br />
+    <!--Php to connect to show if changes were successful-->
+
+    <div class="card-body">
+        <?php
+        if (isset($_SESSION['success']) && $_SESSION['success'] != '') {
+            echo '<h2>' . $_SESSION['success'] . '</h2>';
+            unset($_SESSION['success']);
+        }
+        if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+            echo '<h2>' . $_SESSION['status'] . '</h2>';
+            unset($_SESSION['status']);
+        }
+        ?>
+        <h3 align="center">Faculty Information</h3>
+        <div class="table-responsive">
+            <table id="usersdata" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>Faculty ID</td>
+                        <td>Name</td>
+                        <td>Faculty Type</td>
+                        <td>Faculty Rank</td>
+                        <td>Time Commit</td>
+                        <td>Appointment Date</td>
+                        <td>Department Name</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM department INNER JOIN facultydepartment ON department.deptid = facultydepartment.deptid
+                                INNER JOIN faculty ON facultydepartment.facultyid=faculty.facultyid INNER JOIN user
+                                WHERE faculty.facultyid=user.userid";
+                    $query_run = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_array($query_run)) { ?>
+                        <tr>
+                            <td> <?php echo $row['facultyid']; ?> </td>
+                            <td> <?php echo $row['fname'];
+                                    echo " ";
+                                    echo $row['lname']; ?> </td>
+                            <td> <?php echo $row['facultytype']; ?> </td>
+                            <td> <?php echo $row['facultyrank']; ?> </td>
+                            <td> <?php echo $row['timecommit']; ?> </td>
+                            <td> <?php echo $row['appointmentdate']; ?> </td>
+                            <td> <?php echo $row['deptname']; ?> </td>
+                        </tr> <?php
+                            }
+                                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
+<script>
+    $(document).ready(function() {
+        $('#usersdata').DataTable();
+    });
+</script>
--- navigations/admin/index.php
+++ navigations/admin/index.php
@@ -67,6 +67,7 @@ $row = mysqli_fetch_array($result);
               View Faculty
             </button>
             <ul ul class="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="./adminviewFaculty.php">View Faculty</a></li>
               <li><a class="dropdown-item" href="./adminviewFacultyPart.php">View Part-Time Faculty</a></li>
               <li><a class="dropdown-item" href="./adminviewFacultyFull.php">View Full-Time Faculty</a></li>
               <li><a class="dropdown-item" href="./adminviewFacultyHistory.php">View Faculty History</a></li>
