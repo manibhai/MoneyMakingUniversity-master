b6350bdf07bba7cf069c15edc828e776b1b79b85
Maninder Singh <smaninder403@gmail.com>
2022-12-15 23:35:24 -0500

    added hold on student home page

--- navigations/student/index.php
+++ navigations/student/index.php
@@ -57,7 +57,25 @@ $row = mysqli_fetch_array($result);
 
   <div class="has-bg-img bg-purple bg-blend-screen" style="text-align: center">
     <img class="bg-img" style="text-align: center;" src="https://www.coolgenerator.com/Data/Textdesign/202212/1b7f9d7944d14153613a3187731dd543.png">
-</div>
+  </div>
+  <?php
+  $currUser = $_SESSION['id'];
+  $query = "SELECT * FROM hold INNER JOIN studenthold ON hold.holdname = studenthold.holdid WHERE studenthold.studentid = '$currUser'";
+  $query_run = mysqli_query($connection, $query);
+  $hold = mysqli_fetch_array($query_run);
+  $hname = $hold['holdname'];
+  $htype = $hold['holdtype'];
+  $hdesc = $hold['holddesc'];
+  $hdate = $hold['dategiven'];
+  if ($hold) {
+    echo "<p align='center'> <font color=blue size='6pt'> Hold Name:</font> <font color=red size='6pt'>$hname</font></p>";
+    echo "<p align='center'> <font color=blue size='6pt'> Hold Type:</font> <font color=red size='6pt'>$htype</font></p>";
+    echo "<p align='center'> <font color=blue size='6pt'> Hold Description:</font> <font color=red size='6pt'>$hdesc</font></p>";
+    echo "<p align='center'> <font color=blue size='6pt'> This hold was assigned on:</font> <font color=red size='6pt'>$hdate</font></p>";
+  } else {
+    echo "<p align='center'> <font color=red size='6pt'>NO</font> <font color=blue size='6pt'>Holds on your Account</font></p>";
+  }
+  ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
